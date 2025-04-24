01bd69951817647376c4ff794b0974c5129f677a
danil.zaikin <danil.zaikin@rtksoftlabs.ru>
2025-04-24 13:40:15 +0700

    HW-21: ревью

--- src/main/java/com/danya/mdm/listener/MdmPhoneChangeListener.java
+++ src/main/java/com/danya/mdm/listener/MdmPhoneChangeListener.java
@@ -2,9 +2,8 @@ package com.danya.mdm.listener;
 
 import com.danya.mdm.dto.ChangePhoneDto;
 import com.danya.mdm.service.ValidationService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.danya.mdm.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -17,15 +16,14 @@ import org.springframework.stereotype.Service;
 @ConditionalOnProperty(prefix = "mdm.kafka.send-mdm-in", name = "enabled", havingValue = "true")
 public class MdmPhoneChangeListener {
 
-    private final ObjectMapper objectMapper;
     private final ValidationService validationService;
+    private final JsonUtil jsonUtil;
 
-    @SneakyThrows
     @KafkaListener(topics = "${mdm.kafka.send-mdm-in.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void receiveResponse(ConsumerRecord<String, String> consumerRecord) {
         log.info("Получено сообщение из топика {}", consumerRecord.topic());
 
-        ChangePhoneDto dto = objectMapper.readValue(consumerRecord.value(), ChangePhoneDto.class);
+        ChangePhoneDto dto = jsonUtil.fromJson(consumerRecord.value(), ChangePhoneDto.class);
         validationService.validate(dto);
     }
 }
